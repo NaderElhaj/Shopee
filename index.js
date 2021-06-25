f8bd7adeffec4182a6540e1b7ec384c6645a8bdb
Nader Elhaj <naderelhaj3@gmail.com>
2021-06-25 17:46:58 +0200

    Done With Main Page

--- index.js
+++ index.js
@@ -20,4 +20,49 @@ $(document).ready(function(){
             }
         }
     })
+
+    // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
+
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items : 1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    //blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items : 1
+            },
+            600:{
+                items:3
+            },
+
+        }
+    })
 })

3d2de13608986d45d3408c3e735ba958a3fc658e
Nader Elhaj <naderelhaj3@gmail.com>
2021-06-27 20:16:21 +0200

    quantity in Cart page Perfectly Functional

--- index.js
+++ index.js
@@ -69,19 +69,20 @@ $(document).ready(function(){
     //Product Qty
     let $qty_up = $(".qty .qty-up")
     let $qty_down = $(".qty .qty-down")
-    let $input=$(".qty .qty-input")
 
     $qty_up.click(function(e){
-        if($input.val() >=1 && $input.val() <=9){
-            $input.val(function(i,oldval){
-                return ++oldval
+        let $input = $(`.qty-input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
             })
         }
     })
     $qty_down.click(function(e){
-        if($input.val() >1 && $input.val() <=10){
-            $input.val(function(i,oldval){
-                return --oldval
+        let $input = $(`.qty-input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
             })
         }
     })
